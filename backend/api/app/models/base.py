0d36f7c4198f7c4f95bfee24ed4812afd63a506a
Agustin Nazer <agustin.nazer@hotmail.com>
2026-02-28 19:22:30 -0300

    fixed timestamp - migra a server_default+timezone

--- backend/api/app/models/base.py
+++ backend/api/app/models/base.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from uuid import uuid4
 from sqlalchemy import Column, DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 
 class UUIDAsPrimaryKeyMixin:
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
 
 class TimestampMixin:
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=datetime.utcnow, nullable=False)
